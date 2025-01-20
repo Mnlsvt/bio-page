eaa1e5e50157f58bea8fe0ce76cdb89ee04a90ac
MnLsVt <manolis277@gmail.com>
2025-01-20 12:51:59 +0200

    fixed images issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,28 +161,28 @@ function TimelineDemo() {
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png"
+              src={`https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png`}
               alt="hero template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png"
+              src={`https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png`}
               alt="feature template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png"
+              src={`https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png`}
               alt="bento template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png"
+              src={`https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png`}
               alt="cards template"
               width={500}
               height={500}
@@ -218,28 +218,28 @@ function TimelineDemo() {
           </div>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png"
+              src={`https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png`}
               alt="hero template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png"
+              src={`https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png`}
               alt="feature template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png"
+              src={`https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png`}
               alt="bento template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png"
+              src={`https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/cv-image.png`}
               alt="cards template"
               width={500}
               height={500}
@@ -274,8 +274,7 @@ const people = [
     id: 1,
     name: "Python",
     designation: "",
-    image:
-      "https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/python.png",
+    image: "https://raw.githubusercontent.com/Mnlsvt/bio-page/main/public/python.png",
   },
   {
     id: 2,
